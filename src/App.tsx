3547e11ddf29e52ac5f52647c6b8a810c59cbd65
Minh Ta <minhvinhta@yahoo.com>
2023-12-30 13:47:33 +1000

    * Update font-family * Update README.md * Minor layout update

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         className="d-flex align-items-center justify-content-center vh-100"
       >
         <div
-          className="border border-1 border-secondary bg-secondary-subtle p-3 rounded-3"
+          className="border border-1 border-secondary bg-white p-3 rounded-3"
           style={{ width: 500 }}
         >
           <Header />
@@ -24,12 +24,15 @@ function App() {
               <PadBank />
             </div>
             <div className="col-6 col-md-4">
-              <div
-                className="controls-container w-100 border border-1 border-secondary bg-white px-3 py-2 text-center rounded-3"
-                style={{ minHeight: 45 }}
+              <button
+                className="controls-container w-100 border border-1 border-secondary btn bg-secondary-subtle text-center rounded-3"
+                onClick={(e) => {
+                  e.preventDefault()
+                }}
+                style={{ cursor: "default", minHeight: 37.33 }}
               >
                 <Display />
-              </div>
+              </button>
             </div>
           </div>
 
